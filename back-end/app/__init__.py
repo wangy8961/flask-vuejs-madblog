c6c7f714fdd5a8dacb8aa78caff4126a87c2b45c
WY-Acer <wangy8961@163.com>
2018-11-01 21:34:10 +0800

    2. Vue.js通过axios访问Flask RESTful API

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # enable CORS
+    CORS(app)
 
     # 注册 blueprint
     from app.api import bp as api_bp
